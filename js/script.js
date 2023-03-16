8b0f9619b15eeba9e82b2e1671c180b4776ec184
crisardev <119649715+crisardev@users.noreply.github.com>
2023-03-16 18:49:10 -0300

    subida del proyecto

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function cambiarFondo() {
+    document.getElementById("cuerpoWeb").style.backgroundImage = "url(img/fondo2.jpg)";
+    document.getElementById("info").style.color = "black";
+    document.getElementById("exp").style.color = "black";
+    document.getElementById("hab").style.color = "black";
+}
+
